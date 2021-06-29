ee282a9ee7079c6ee0ce0f2609f59f3c7b9b508e
Oyedee <oyempemia@gmail.com>
2021-06-29 20:11:57 +0100

    commit code changes on activiy UI and functionality

--- app/src/main/java/com/example/firstkotlinapp/MainActivity.kt
+++ app/src/main/java/com/example/firstkotlinapp/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.firstkotlinapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button_id)
+        val diceSides = findViewById<TextView>(R.id.textView_sides)
+        val diceRoll = findViewById<TextView>(R.id.textView_roll)
+
+        button?.setOnClickListener(){
+            val myFirstDice = Dice()
+            diceSides.setText(myFirstDice.sides.toString())
+
+            val diceNumber = myFirstDice.roll().toString()
+
+            diceRoll.setText(diceNumber)
+
+        }
     }
 }
